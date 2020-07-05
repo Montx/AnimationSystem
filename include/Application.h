a0e508b5430aecc74443d9a6fa71d04d2e1dab41
Ricardo <fateweaving@hotmail.com>
2020-07-05 22:17:12 +0200

    Win32 + OpenGl Context + Game Loop Implementation

---
+++ include/Application.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Application {
+public:
+
+	Application(const Application&) = delete;
+	Application& operator=(const Application&) = delete;
+
+	inline Application() { }
+
+	inline virtual ~Application() { }
+
+	inline virtual void Initialize() { }
+
+	inline virtual void Update(float inDeltaTime) { }
+
+	inline virtual void Render(float inAspectRatio) { }
+
+	inline virtual void Shutdown() { }
+};
